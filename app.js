87c065da1dea58070cd24102372fd8cc1a8e5ea8
Sunnylin0320 <sunny8503201@gmail.com>
2023-11-09 09:52:42 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // 設定 port 3000
 app.listen(3000, () => {
