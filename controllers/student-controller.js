8d7d2cc67fab7f9c7961a679ba25baad6323e3ca
Sunnylin0320 <sunny8503201@gmail.com>
2023-11-14 13:58:35 +0800

    feat: student get course

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -5,7 +5,18 @@ const studentController = {
     Course.findAll({
       raw: true
     })
-      .then(courses => res.render('courses', { courses }))
+      .then(courses => res.render('students/courses', { courses }))
+      .catch(err => next(err))
+  },
+  getCourse: (req, res, next) => {
+    Course.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(course => {
+        if (!course) throw new Error("Course didn't exist!")
+
+        res.render('students/course', { course })
+      })
       .catch(err => next(err))
   }
 }
--- routes/modules/students.js
+++ routes/modules/students.js
@@ -5,5 +5,6 @@ const studentController = require('../../controllers/student-controller')
 
 // student 10支
 router.get('/courses', studentController.getCourses) // User查看所有Courses
+router.get('/courses/:id', studentController.getCourse) // User查看單筆Course
 
 module.exports = router
