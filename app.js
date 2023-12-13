4fe7caa713ddc27eef6b24cd9bc4ce9c9c5ad07a
Sunnylin0320 <sunny8503201@gmail.com>
2023-12-13 14:41:37 +0800

    feat: add banner

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
 const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
@@ -15,6 +17,9 @@ const dayjsHelpers = require('./helpers/dayjs-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
 const port = process.env.PORT || 3000
 
 const hbs = exphbs({
@@ -26,6 +31,7 @@ const hbs = exphbs({
 })
 app.engine('hbs', hbs)
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(
   session({
@@ -47,6 +53,19 @@ app.use((req, res, next) => {
   next()
 })
 app.use(routes)
+
+io.on('connection', socket => {
+  console.log('A user connected')
+
+  socket.on('chat message', message => {
+    io.emit('chat message', message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
+})
+
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
